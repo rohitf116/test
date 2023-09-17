cfe95439ecd092a95fecff74c72e4d1fef734113
sunil <suniltiwari460@gmail.com>
2023-09-17 14:40:07 +0530

    update

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -20,56 +20,6 @@ export const createQuestions = async (req, res) => {
   }
 };
 
-// export const getQuestions = async (req, res) => {
-//   try {
-//     const roomId = req.params.roomId; // Assuming you want to get roomId from the URL parameter
-//     const count = 5;
-
-//     // Check if roomId already exists in any exam object
-//     const existingExam = await examModel.findOne({ roomId });
-
-//     if (existingExam) {
-//       return res.status(400).json({
-//         success: false,
-//         message: "Exam with the same roomId already exists",
-//       });
-//     }
-
-//     // Get 5 random questions
-//     const randomQuestions = await questionModel.aggregate([
-//       { $sample: { size: count } },
-//     ]);
-
-//     // Create a new exam document with the users, room ID, and random question IDs
-//     const exam = new examModel({
-//       roomId: roomId,
-//       questions: randomQuestions.map((question) => question._id), // Extract question IDs
-//     });
-
-//     // Save the exam document to the Exam collection
-//     await exam.save();
-
-//     // console.log(exam, "......exam");
-
-//     // Format the response as desired
-//     return res.status(201).json({
-//       success: true,
-//       message: "A new Exam has been created",
-//       data: {
-//         roomId: roomId,
-//         questions: randomQuestions,
-//       },
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({
-//       success: false,
-//       message: "Server error",
-//       error: error.message,
-//     });
-//   }
-// };
-
 export const getQuestions = async (req, res) => {
   try {
     const roomId = req.params.roomId;
